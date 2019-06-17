d5774e0c92cb542246429f686f4d4839edbd2841
Balmore <ike24dc@gmail.com>
2019-06-16 21:14:49 -0600

    3- servidor despliega

--- pi/urls.py
+++ pi/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = 'pi'
+
 urlpatterns=[
-    path('',views.index,name='index'),
+    #path(' ',views.index,name = 'index'),
+    path(' ', views.IndexView.as_view(), name = 'index'),
+    #no se si el espacio en la comilla simple puede causar algo
+
+
+
+
+
+    #ejemplo: /pi/2
+    path('<int:pizza_id>/',views.home, name='home'),
 ]
--- pi/views.py
+++ pi/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse	#n
+from django.template import loader	#n
+from django.http import Http404	#n
+from .models import Pizza
+
+from django.views import generic
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hola:(")
+class IndexView(generic.ListView):
+	template_name='pi/index.html'
+	context_object_name='pizza_list'
+
+	def get_queryset(self):
+		return Pizza.objects.order_by(pizza_id)
+
+
+
+
+
+#nuevo
+def home(request, pizza_id):
+	pizza=get_object_or_404(Pizza, pk=pizza_id)
+	#return HttpResponse("estas mirando la pizza: %s." % pizza_id)
+	return render(request,'pi/home.html', {'pizza':pizza})
+
