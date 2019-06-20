6773ef15fb59542aa3f937331c289cf841257aba
Balmore <ike24dc@gmail.com>
2019-06-19 22:29:57 -0600

    7- otro intento de login

--- pi/urls.py
+++ pi/urls.py
@@ -5,11 +5,12 @@ app_name = 'pi'
 
 urlpatterns=[
     #path(' ',views.index,name = 'index'),
+    #esto es por defecto pi/
     path('', views.IndexView.as_view(), name = 'index'),
     #no se si el espacio en la comilla simple puede causar algo
 
     #login view. Commment
-    path('',views.LoginView.as_view(), name='login'),
+    path('<slug:"login">/',views.LoginView.as_view(), name='login'),
 
     #ejemplo: /pi/2
     path('<int:pk>/',views.HomeView.as_view(), name='home'),
